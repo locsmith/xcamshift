125a11d3eb6e866497bb6a068fc00bac82609854
varioustoxins <garyt@bmb.leeds.ac.uk>
2012-01-17 09:32:12 +0000

    implementation of dihedral parameter table

--- xcamshift/src/dihedral_table.py
+++ xcamshift/src/dihedral_table.py
@@ -5,10 +5,7 @@ Created on 30 Dec 2011
 '''
 from keys import Atom_key
 
-
-        
-class Dihedral_table(object):
-    
+class Dihedral_table_base(object):
     DATA = 'data'
 
     EXPONENT = 'exponent'
@@ -78,7 +75,12 @@ class Dihedral_table(object):
             result.append(tuple(atom_keys))
             
         return result
-            
+        
+class Dihedral_table(Dihedral_table_base):
+    
+
+    def __init__(self,table):   
+        super(Dihedral_table, self).__init__(table)    
             
     
     def get_dihedral_shift(self,target_atom,dihedral_key):
@@ -95,3 +97,41 @@ class Dihedral_table(object):
                 result  = data[target_atom]
 #            
         return result
+
+class Dihedral_parameter_table(Dihedral_table_base):
+    
+    PARAMETERS='parameters'
+    
+
+    def __init__(self,table):   
+        super(Dihedral_parameter_table, self).__init__(table)
+
+
+    def get_parameters(self):
+        return self.table[self.PARAMETERS]
+    
+    
+    def check_parameter_key(self, parameter):
+        parameters = self.get_parameters()
+        if not parameter in parameters:
+            message = "parameter %s not in parameters %s" % (parameter,', '.join(parameters))
+            raise KeyError(message)
+    
+    
+    def get_dihedral_parameter(self,target_atom,dihedral_key,parameter):
+        self._check_target_atom(target_atom)
+        self._check_dihedral_key(dihedral_key)
+        self.check_parameter_key(parameter)
+        
+        result = None
+        
+        raw_dihedral_key = dihedral_key.get_dihedral_key()
+
+        if raw_dihedral_key in self.table[self.DATA]:
+            data = self.table[self.DATA][raw_dihedral_key]
+            if parameter in data:
+                parameter = data[parameter]
+                if target_atom in parameter:
+                    result  = parameter[target_atom]
+#            
+        return result    
