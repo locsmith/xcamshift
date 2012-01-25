75e094d9695d6a5f5e99b85266ad3887ee54502b
varioustoxins <garyt@bmb.leeds.ac.uk>
2012-01-25 12:57:42 +0000

    add rudimentary manually derived test suite

---
+++ xcamshift/src/test/test_suite.py
@@ -0,0 +1,23 @@
+'''
+Created on 25 Jan 2012
+
+@author: garyt
+'''
+import unittest2
+from test.test_segment_manager import Test_segment_manager
+from test.test_observed_chemical_shifts import TestObservedShiftTable
+from test.test_xcamshift import TestXcamshift
+
+
+def load_tests(loader, tests, pattern):
+    suite = unittest2.TestSuite()
+    suite.addTests(tests = loader.loadTestsFromTestCase(TestObservedShiftTable))
+    suite.addTests(tests = loader.loadTestsFromTestCase(Test_segment_manager))
+    suite.addTests(tests = loader.loadTestsFromTestCase(TestXcamshift))
+    suite.addTests(tests = loader.loadTestsFromTestCase(TestObservedShiftTable))
+    return suite
+
+if __name__=="__main__":
+    
+    unittest2.main(verbosity=10)
+    
