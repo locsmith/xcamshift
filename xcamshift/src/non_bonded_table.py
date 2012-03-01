7ad9a8d6acbcfda212a37bc1b03481feab696dec
varioustoxins <garyt@bmb.leeds.ac.uk>
2012-03-01 21:04:50 +0000

    add non bonded table!

---
+++ xcamshift/src/non_bonded_table.py
@@ -0,0 +1,70 @@
+'''
+Created on 19 Jan 2012
+
+@author: garyt
+'''
+
+
+class Non_bonded_table(object):
+
+
+    TARGET_ATOMS = "target_atoms"
+    SPHERE_1 = 'sphere_1'
+    SPHERE_2 = 'sphere_2'
+    EXPONENT = 'exponent'
+    DATA = 'data'
+    
+    def __init__(self, table):
+        self._table = table
+    
+    def get_exponent(self,sphere):
+        self._check_sphere(sphere)
+        return self._table[self.DATA][sphere][self.EXPONENT]
+    
+    def get_target_atoms(self):
+        return self._table[self.TARGET_ATOMS]    
+
+    def _check_target_atom(self, atom_name):
+        atoms = self.get_target_atoms()
+        if atom_name not in atoms:
+            template = "atom_name %s is not in sidechain shift _table target atoms (%s)"
+            message = template % (atom_name, ', '.join(atoms))
+            raise KeyError(message)
+    
+    def get_spheres(self):
+        spheres = self._table[self.DATA].keys()
+        spheres.sort()
+        return tuple(spheres)
+    
+    def _check_sphere(self,sphere):
+        spheres = self.get_spheres()
+        if sphere not in spheres:
+            template = "sphere %s is not in non bonded table spheres (%s)"
+            message = template % (sphere, ', '.join(spheres))
+            raise KeyError(message)
+    
+    def _check_coefficient_key(self,sphere,coefficent_key):
+        self._check_sphere(sphere)
+        coefficent_keys = self.get_coefficent_keys(sphere)
+        if coefficent_key not in coefficent_keys:
+            template = "coefficient key %s is not in non bonded table coefficient keys (%s)"
+            coefficent_keys_string = [`coefficent_key` for coefficent_key in coefficent_keys]
+            message = template % (coefficent_key, ', '.join(coefficent_keys_string))
+            raise KeyError(message)
+    
+    def get_coefficent_keys(self, sphere):
+        self._check_sphere(sphere)
+        coefficent_keys = self._table[self.DATA][sphere].keys()
+        coefficent_keys.remove('exponent')
+        coefficent_keys.sort()
+        return tuple(coefficent_keys) 
+    
+    def get_non_bonded_coefficient(self,target_atom,sphere,remote_atom_type,hybridisation):
+        self._check_target_atom(target_atom)
+        self._check_sphere(sphere)
+        
+        coefficent_key = remote_atom_type,hybridisation
+        self._check_coefficient_key(sphere,coefficent_key)
+        
+        return self._table[self.DATA][sphere][coefficent_key][target_atom]
+    
