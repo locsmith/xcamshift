bf1a3de3ae06350ddfe69f3e397023b12165c981
garyt <garyt.and.sarahb@gmail.com>
2012-04-15 19:26:08 +0100

    major update add output to files and options including verbose

--- xcamshift/src/table_builders/xcamshift/camshift_table_builder.py
+++ xcamshift/src/table_builders/xcamshift/camshift_table_builder.py
@@ -9,8 +9,8 @@ Created on 30 Mar 2012
 '''
 import sys
 import os
-import glob
 import re
+import glob
 from table_builders.xcamshift.Backbone_distance_extractor import BB_table_extractor
 from table_builders.xcamshift.Extra_distance_extractor import XTRA_table_extractor
 from table_builders.xcamshift.Sidechain_distance_extractor import SC_table_extractor
@@ -18,6 +18,12 @@ from table_builders.xcamshift.Dihdedral_distance_extractor import DIHEDRALS_tabl
 from table_builders.xcamshift.Ring_table_extractor import RING_table_extractor
 from table_builders.xcamshift.Nonbonded_table_extractor import Nonbonded_table_extractor
 from common_constants import CAMSHIFT_SUB_POTENTIALS
+import argparse
+
+
+FAILURE = -1
+VERSION  = '1.0.0'
+CAMSHIFT_VERSION = (1,35,0)
 
 def get_atom_type_for_filename(file_path):
     file_name = os.path.split(file_path)[1]
@@ -25,16 +31,6 @@ def get_atom_type_for_filename(file_path):
     match = re.search(file_pattern, file_name)
     return match.group(1)
     
-def check_args():
-    
-    args = []
-    if len(sys.argv) > 1:
-        args = sys.argv[1:]
-    if len(args) != 1:
-        template = "Camshift_table_builder requires a single directory as its argument but got '%s'"
-        print >> sys.stderr, template % `args`
-        print >> sys.stderr, "exiting..."
-        sys.exit(-1)
         
 def parameter_file_iterator(table_dir, template = '1[A-Z]*orgpairs.par*'):
     if not os.path.isabs(table_dir):
@@ -64,8 +60,8 @@ class Camshift_table_reader(object):
     def read(self):
         self._find_files(self.table_dir)
         
-        for file_type in self.file_types:
-            self._parse_files(file_type)
+        for residue_type in self.file_types:
+            self._parse_files(residue_type)
 
     def _split_files_by_extension(self,par_files):
         for par_filename in par_files:
@@ -83,9 +79,9 @@ class Camshift_table_reader(object):
         self._split_files_by_extension(par_files)
     
     
-    def _parse_files(self,file_type):
-        self.data.setdefault(file_type,{})
-        for file_path in self.files_by_type[file_type]:
+    def _parse_files(self,residue_type):
+        self.data.setdefault(residue_type,{})
+        for file_path in self.files_by_type[residue_type]:
             atom_type = get_atom_type_for_filename(file_path)
             file_h = open(file_path)
             
@@ -100,18 +96,22 @@ class Camshift_table_reader(object):
                 table_type = items[0]
                 
                 values = [float(item) for item in items[1:]]
-                self.data[file_type].setdefault(table_type, {})[atom_type] = values
+                self.data[residue_type].setdefault(table_type, {})[atom_type] = values
         
-    
+
+            
     def get_file_types(self):
         return self.file_types
+    
+    def get_number_files(self):
+        return len(self.data)
 
 
-def _get_file_type_name(file_type):
-    if file_type == "":
+def _get_file_type_name(residue_type):
+    if residue_type == "":
         file_type_name = 'base'
     else:
-        file_type_name = file_type
+        file_type_name = residue_type
     
     return file_type_name
 
@@ -163,29 +163,135 @@ def extract(data_dir, sub_potential = None, residue_types=None):
     
     
     
+def build_output_name(sub_potential_name, residue_type, CAMSHIFT_VERSION, template='cams_%s_%s_%s.yaml', version_template = '%i_%i_%i'):
+    version_string  = version_template % CAMSHIFT_VERSION
+    
+    sub_potential_name = sub_potential_name.lower().strip()
+    
+    if residue_type == '':
+        residue_type='base'
+    return template % (version_string, sub_potential_name,residue_type) 
+
+
+
+def build_args_parser():
+    message = 'Application to parse camshift data tables to yaml for xcamshift'
+    
+    parser = argparse.ArgumentParser(description=message,version='1.0')
+    
+    parser.add_argument('directory', action="store")
+    parser.add_argument('--verbose', action="store_true", default=False)
+    parser.add_argument('-o', '--output', default="-", help="where to put the files either a file system path or - for stdout") 
+    
+    return parser.parse_args()
+
+
+
+
+
+def make_output_directory_or_exit(output_dir):
+    if not os.path.isdir(output_dir):
+        if os.path.isfile(output_dir):
+            print >> sys.stderr, 'output directory (%s) cannot be a file!' % output_dir
+            print >> sys.stderr, 'exiting...'
+            sys.exit(FAILURE)
+        os.mkdir(output_dir)
+    if not os.path.isdir(output_dir):
+        print >> sys.stderr, 'cannot create output directory (%s)!' % output_dir
+        print >> sys.stderr, 'exiting...'
+        sys.exit(FAILURE)
+        
+
+
+
+def write_files(output_data, output_path):
+    file_handle = None
+    try:
+        file_handle = open(output_path, 'w')
+        print >> file_handle, output_data
+        file_handle.close()
+    except:
+        file_handle.close()
+
+
+def get_human_readable_file_types(file_types):
+    result = []
+    for file_type in file_types:
+        if file_type == '':
+            result.append('base')
+        else:
+            result.append(file_type)
+    return result       
+
+
     
 if __name__ == '__main__':
 
-    check_args()
     
-    table_dir = sys.argv[1]
+    args = build_args_parser()
+    
+    table_dir = args.directory
+    
+    if args.verbose:
+        print >> sys.stderr, 'camshift_table_builder, version %s' % VERSION
+        print >> sys.stderr
+        print >> sys.stderr, 'reading files from %s' % table_dir
+    
     
     reader = _read_data_into_reader(table_dir)
     
-    file_types = reader.get_file_types()
+    residue_types = reader.get_file_types()
     
     table_types = CAMSHIFT_SUB_POTENTIALS
+    
+    table_type_names = [table_type.strip().upper() for table_type in table_types]
+    files_to_output = len(table_type_names) * len(residue_types)
+    
+    if args.verbose:
+        print >> sys.stderr, '  read %i files' % reader.get_number_files()
+        print >> sys.stderr, '  camshift version %i.%i.%i' % CAMSHIFT_VERSION
+        print >> sys.stderr, '  residue types are % s' % ','.join(get_human_readable_file_types(residue_types))
+        print >> sys.stderr, '  sub potentials are %s' % ', '.join(table_type_names)
+        print >> sys.stderr, '  output directory is %s' % args.output
+        print >> sys.stderr, '  there are %i files to produce...' % (files_to_output)
+        print >> sys.stderr, ''
+    
+    
+    STDOUT = '-'
+    if args.output != STDOUT:
+        make_output_directory_or_exit(args.output)    
     
     #TODO use extract function here
     extractor_classes = _get_extractor_classes()
 
+    count = 1
     for extractor_class, table_type in zip(extractor_classes,table_types):
         extractor = extractor_class(reader.data)
         
-        for file_type in file_types:
-            file_type_name  = _get_file_type_name(file_type)
+        for residue_type in residue_types:
+            file_type_name  = _get_file_type_name(residue_type)
             
-            print "\n--- %s %s ---\n" % (table_type, file_type_name)
-    
-#            extractor = BB_table_extractor(reader.data)
-            print extractor.extract(file_type)
+            sub_potential_name  = extractor.get_name()
+            
+            output_data = extractor.extract(residue_type)
+            
+            
+            if args.output == STDOUT:
+                title = build_output_name(sub_potential_name, residue_type, CAMSHIFT_VERSION, 
+                                          template='camshift %s - %s - %s', version_template='%s.%s.%s')
+                print '----- %s ------' % title
+                
+            else:
+                output_filename = build_output_name(sub_potential_name, residue_type, CAMSHIFT_VERSION)
+                
+                if args.verbose:
+                    print >> sys.stderr, '%i of %i. %s' % (count, files_to_output,  output_filename) 
+                    
+                output_path = os.path.join(args.output,output_filename)
+                
+                write_files(output_data, output_path)
+                count += 1
+    if args.verbose:
+        print >> sys.stderr, ''
+        print >> sys.stderr, 'done.'
+        
