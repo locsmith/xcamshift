46e79649b86b8a75b3ae71b8bc282084c60aa94a
varioustoxins <garyt@bmb.leeds.ac.uk>
2012-04-27 08:47:26 +0100

    now correctly outputs formatted dicts as strings
    this version also 
    
    1. filters out first and last residues
    2. has a much cleaner internal representation

--- xcamshift/src/camshift2shifts.py
+++ xcamshift/src/camshift2shifts.py
@@ -7,15 +7,23 @@ Created on 24 Apr 2012
 
 
 
+
+
+
+
+
 '''
 import sys
 import re
 from collection_backport import OrderedDict
+from itertools import ifilter
+from functools import partial
 
 class Camshift_output_parser(object):
     
     def __init__(self, source):
         self._source  = source
+
         
     def _get_num_residues_or_raise(self,lines):
         residues = None
@@ -45,10 +53,10 @@ class Camshift_output_parser(object):
         try:
             lines = file_h.readlines()
             
-            residues = self._get_num_residues_or_raise(lines)
+            number_residues = self._get_num_residues_or_raise(lines)
             
-            shifts = lines[-residues:]
-            header = lines[-residues - 2]
+            shifts = lines[-number_residues:]
+            header = lines[-number_residues - 2]
             atom_types = header.strip().split()[2:]
             
             result = OrderedDict()
@@ -57,17 +65,69 @@ class Camshift_output_parser(object):
                 segid = ''
                 residue_number = elems[0]
                 for atom, shift_string in zip(atom_types, elems[2:]):
-                    key = segid, residue_number, atom
+                    key = segid, int(residue_number), atom
                     result[key] = float(shift_string)
         finally:
             self._close(file_h)
             
         return result
-        
+
+def get_first_and_last(data):
+    residue_numbers = set()
+    
+    for segid,residue_number,atom in data:
+        residue_numbers.add(residue_number)
+    residue_numbers = list(residue_numbers)
+    residue_numbers.sort()
+    
+    return residue_numbers[0],residue_numbers[-1]
+
+def remove_first_and_last_by_residue(data):
+    keys = data.keys()
+    keys.sort()
+    first_and_last = get_first_and_last(data)
+    
+    residue_filter =  partial(ifilter, lambda x:not x[1] in first_and_last)
+    
+    return OrderedDict([(key, data[key]) for key in residue_filter(keys)])
+
+def format_as_atom_selection_float_dict(data, name = 'test', float_format = '9.5f'):
+    data_keys = []
+    values = []
+    for data_key in data:
+        data_keys.append("    ('%s',%i,'%s')" % data_key)
+        values.append(data[data_key])
+    
+    max_width = reduce(get_longest_length, data_keys, 0)
+    string_format = "%%-%is  :  %%%s," % (max_width, float_format)
+    
+    result = []
+    
+    result.append( '%s  = {' % name)
+    for data_key_value in zip(data_keys, values):
+        result.append(string_format % data_key_value)
+    result.append('}')
+    
+    return '\n'.join(result)
+
+def get_longest_length(accumulator ,value):
+    result = accumulator
+    
+    value_length = len(value)
+    
+    if value_length > accumulator: 
+        result = value_length
+    
+    return result 
+    
+
 if __name__ == '__main__':
     
     parser = Camshift_output_parser(sys.argv[1])
-    print parser.parse()
-   
+
+    data  = parser.parse()
+    data  = remove_first_and_last_by_residue(data)
+    
+    print format_as_atom_selection_float_dict(data)
             
         
