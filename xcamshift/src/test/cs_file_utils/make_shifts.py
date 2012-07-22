a97978a45df52fe7bd8d7d59f294ce755b1dff75
varioustoxins <garyt@bmb.leeds.ac.uk>
2012-07-22 16:19:31 +0100

    add a simple utility for making camshift chemical shifts file sets
    currently on makes files with shifts of 0.00001 ppm
    
    note don't use 0.0 ppm as this is defined as undefined by camshift !

---
+++ xcamshift/src/test/cs_file_utils/make_shifts.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+'''
+Created on 21 Jul 2012
+
+@author: garyt
+'''
+
+from common_constants import h_keys
+import os
+
+class Make_zero_shifts(object):
+    '''
+    classdocs
+    '''
+
+    template = '%sshifts.dat'
+
+    def __init__(self, path, length, glycines=()):
+        '''
+        Constructor
+        '''
+        self.path = path
+        self.range = [2,length]
+        self.glycines=glycines
+        
+    def write(self):
+        for atom_type in h_keys:
+            file_name = self.template % atom_type
+            file_path = os.path.join(self.path,file_name)
+            
+            file_h = open(file_path,'w+')
+            
+            for i in range(*self.range):
+                if atom_type == 'CB' and i in self.glycines:
+                    continue
+                print >> file_h, "%-4i %9.8f" % (i, 0.000001) 
+            
+            file_h.close()
+        
+if __name__ == "__main__":
+    path = 'data'
+    
+    shift_maker = Make_zero_shifts(path,56,(9,14,38,41))
+    shift_maker.write()
+
+    
