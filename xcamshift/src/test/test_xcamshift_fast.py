f6729dce8ce9e3d15f82f286023940d46acbecfb
varioustoxins <garyt@bmb.leeds.ac.uk>
2012-08-01 10:14:22 +0100

    test_xcamshift can now tests potentials using fast calculators

---
+++ xcamshift/src/test/test_xcamshift_fast.py
@@ -0,0 +1,11 @@
+'''
+Created on 1 Aug 2012
+
+@author: garyt
+'''
+from test import test_xcamshift
+
+if __name__ == '__main__':
+    test_xcamshift.fast=True
+    print 'here'
+    test_xcamshift.run_tests()
