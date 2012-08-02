b2e3b9afaa203b9b797ca74af030766bfa9b7008
varioustoxins <garyt@bmb.leeds.ac.uk>
2012-08-02 21:54:10 +0100

    add testing of fast version of force field as an option

---
+++ xcamshift/src/test/test_xcamshift_gb3_fast.py
@@ -0,0 +1,10 @@
+'''
+Created on 1 Aug 2012
+
+@author: garyt
+'''
+from test import test_xcamshift_gb3
+
+if __name__ == '__main__':
+    test_xcamshift_gb3.fast=True
+    test_xcamshift_gb3.run_tests()
