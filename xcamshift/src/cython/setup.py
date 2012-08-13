4a3894554d11a2130b092b0ec1ef1345dc067e8e
varioustoxins <garyt@bmb.leeds.ac.uk>
2012-08-13 15:36:21 +0100

    add ability to turn off bounds checking (disabled)
    did'nt seem to make much difference in speed...
    
    suggests that other things are much more important, e.g. 
    1. remaining python code  structures
    2. the fact that we are a class 
    3. are the bits we asked to be inline inlined

--- xcamshift/src/cython/setup.py
+++ xcamshift/src/cython/setup.py
@@ -7,37 +7,43 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
  
+
+ext_modules = [Extension("test_distance",  ["test_distance.pyx"],
+                         define_macros = [('CPLUSPLUS', '1') ,
+                                          ('USE_CDS_NAMESPACE', '1')],
+                        
+                         language="c++",
+                         
+                         include_dirs=['/home/garyt/programs/xplor-nih/2.31.0/common',
+                                       '/home/garyt/programs/xplor-nih/2.31.0/CDSlib',
+                                       '/home/garyt/programs/xplor-nih/2.31.0/arch/Linux_i686/include']),
+               
+               Extension("shift_calculators",  ["Cython_shift_calculator.pyx"],
+                         define_macros = [('CPLUSPLUS', '1') ,
+                                          ('USE_CDS_NAMESPACE', '1')],
+                        
+                         language="c++",
+                         
+                         include_dirs=['/home/garyt/programs/xplor-nih/2.31.0/common',
+                                       '/home/garyt/programs/xplor-nih/2.31.0/CDSlib',
+                                       '/home/garyt/programs/xplor-nih/2.31.0/arch/Linux_i686/include']),
+  
+               Extension("fast_segment_manager",  ["Cython_segment_manager.pyx"],
+                         define_macros = [('CPLUSPLUS', '1') ,
+                                          ('USE_CDS_NAMESPACE', '1')],
+                        
+                         language="c++",
+                         
+                         include_dirs=['/home/garyt/programs/xplor-nih/2.31.0/common',
+                                       '/home/garyt/programs/xplor-nih/2.31.0/CDSlib',
+                                       '/home/garyt/programs/xplor-nih/2.31.0/arch/Linux_i686/include'])
+               ]
+
+if False:
+    for e in ext_modules:
+        e.pyrex_directives = {"boundscheck": False}
 setup(
+    ext_modules=ext_modules,
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("test_distance",  ["test_distance.pyx"],
-                             define_macros = [('CPLUSPLUS', '1') ,
-                                              ('USE_CDS_NAMESPACE', '1')],
-                            
-                             language="c++",
-                             
-                             include_dirs=['/home/garyt/programs/xplor-nih/2.31.0/common',
-                                           '/home/garyt/programs/xplor-nih/2.31.0/CDSlib',
-                                           '/home/garyt/programs/xplor-nih/2.31.0/arch/Linux_i686/include']),
-                   
-                   Extension("shift_calculators",  ["Cython_shift_calculator.pyx"],
-                             define_macros = [('CPLUSPLUS', '1') ,
-                                              ('USE_CDS_NAMESPACE', '1')],
-                            
-                             language="c++",
-                             
-                             include_dirs=['/home/garyt/programs/xplor-nih/2.31.0/common',
-                                           '/home/garyt/programs/xplor-nih/2.31.0/CDSlib',
-                                           '/home/garyt/programs/xplor-nih/2.31.0/arch/Linux_i686/include']),
-      
-                   Extension("fast_segment_manager",  ["Cython_segment_manager.pyx"],
-                             define_macros = [('CPLUSPLUS', '1') ,
-                                              ('USE_CDS_NAMESPACE', '1')],
-                            
-                             language="c++",
-                             
-                             include_dirs=['/home/garyt/programs/xplor-nih/2.31.0/common',
-                                           '/home/garyt/programs/xplor-nih/2.31.0/CDSlib',
-                                           '/home/garyt/programs/xplor-nih/2.31.0/arch/Linux_i686/include'])
-                   ]
 )
       
