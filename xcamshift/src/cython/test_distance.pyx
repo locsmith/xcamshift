af280a2e061b096d7c52cd163ff4bea12b450def
varioustoxins <garyt@bmb.leeds.ac.uk>
2012-08-14 22:43:10 +0100

    test changes to for accessing atom positions

--- xcamshift/src/cython/test_distance.pyx
+++ xcamshift/src/cython/test_distance.pyx
@@ -1,16 +1,16 @@
 
-import xcamshift 
+#import xcamshift 
 from protocol import initStruct
 from pdbTool import PDBTool
 cimport xplor_access
-from xplor_access cimport norm,Vec3,currentSimulation
+from xplor_access cimport norm,Vec3,currentSimulation,Coord_holder
 
 def get_distance():
     test_1_2 = Test_distance_1_2()
     return test_1_2.get_distance_1_2()
-
+ 
 cdef struct ring_atom_positions:
-    int num_atoms
+    int num_atoms 
     Vec3 atom_pos_0
     Vec3 atom_pos_1
     Vec3 atom_pos_2
@@ -21,17 +21,21 @@ cdef struct ring_atom_positions:
 cdef Vec3* get_pos_pointer(ring_atom_positions&  positions):
     return &positions.atom_pos_0
 
-cdef class Test_distance_1_2:
+cdef class Test_distance_1_2:  
 
     def setup(self):
         initStruct("test_data/3_ala/3ala.psf")
         PDBTool("test_data/3_ala/3ala.pdb").read()
-         
+          
     cpdef get_distance_1_2(self):
         self.setup()
-        vec1 = currentSimulation().atomPosArr().data(0)
-        vec2 = currentSimulation().atomPosArr().data(1)
+        cdef Coord_holder holder = Coord_holder(currentSimulation()[0])
+        vec1 = holder.getPos(0)
+        vec2 = holder.getPos(1)
+#        vec1 = currentSimulation().atomPosArr().data(0)
+#        vec2 = currentSimulation().atomPosArr().data(1)
         cdef Vec3 result =  vec2 - vec1
+        print vec1.x()
         return norm(result)
 
 cpdef test_vec3_pointer():
