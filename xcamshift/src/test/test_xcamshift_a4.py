9c519bf7c485bf1853c17e68dd0d081ae1369e9f
varioustoxins <garyt@bmb.leeds.ac.uk>
2013-02-17 23:47:55 +0000

    update copyright to include University of Leeds

--- xcamshift/src/test/test_xcamshift_a4.py
+++ xcamshift/src/test/test_xcamshift_a4.py
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2013 gary thompson.
+# Copyright (c) 2013 Gary Thompson & The University of Leeds.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the GNU Lesser Public License v3.0
 # which accompanies this distribution, and is available at
