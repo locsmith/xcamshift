40ff1b33ca5b610335de55d96ee7f72cfd04e4c1
varioustoxins <garyt@bmb.leeds.ac.uk>
2013-02-19 19:08:10 +0000

    add acagaca.py to the test directory

---
+++ xcamshift/src/test/acaggaca.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2013 Gary Thompson & The University of Leeds.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the GNU Lesser Public License v3.0
+# which accompanies this distribution, and is available at
+# http://www.gnu.org/licenses/lgpl-3.0.html
+# 
+# Contributors:
+#     gary thompson - initial API and implementation
+#-------------------------------------------------------------------------------
+
+acaggaca_shifts = {
+
+    ('',2,'C')       :  -0.3709,
+    ('',2,'CA')      :  -2.8517,
+    ('',2,'CB')      :  11.3616,
+    ('',2,'H')       :  -0.1061,
+    ('',2,'HA')      :   0.0116,
+    ('',2,'N')       :  -1.4131,
+    ('',7,'C')       :  -0.3709,
+    ('',7,'CA')      :  -2.8517,
+    ('',7,'CB')      :  11.3616,
+    ('',7,'H')       :  -0.1061,
+    ('',7,'HA')      :   0.0116,
+    ('',7,'N')       :  -1.4131,
+}
