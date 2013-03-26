589e9154c9b871e78c6edb79a5ea6f41cd2b922e
varioustoxins <garyt@bmb.leeds.ac.uk>
2013-03-26 07:41:56 +0000

    more useful set of default tests when developing
    this basically emphasises testing the whole energy and the forces and
    the sub potential force components:  
      1. test_total_forces_and_energy_10_step 
      2. test_force_components
    
    the flags used require python 2.7 but could be replaced by a try catch
    on exit

--- xcamshift/src/test/test_xcamshift_gb3.py
+++ xcamshift/src/test/test_xcamshift_gb3.py
@@ -771,8 +771,9 @@ class TestXcamshiftGB3(unittest2.TestCase):
 def run_tests():
     if fast:
         print >> sys.stderr, TestXcamshiftGB3.__module__,"using fast calculators"
-    unittest2.main(module='test.test_xcamshift_gb3',failfast=True)
-#    unittest2.main(module='test.test_xcamshift_gb3',defaultTest='TestXcamshiftGB3.test_shift_differences')
+#     unittest2.main(module='test.test_xcamshift_gb3',failfast=True)
+    unittest2.main(module='test.test_xcamshift_gb3',defaultTest='TestXcamshiftGB3.test_total_forces_and_energy_10_step', exit=False)
+    unittest2.main(module='test.test_xcamshift_gb3',defaultTest='TestXcamshiftGB3.test_force_components')
 #    unittest2.main(module='test.test_xcamshift',defaultTest='TestXcamshift.test_shift_differences')
 
 
