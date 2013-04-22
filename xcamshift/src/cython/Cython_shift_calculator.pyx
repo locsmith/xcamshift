08d4e3c25e22a1f80daa111a33ff1fa4a29e1af9
varioustoxins <garyt@bmb.leeds.ac.uk>
2013-04-22 08:13:13 +0100

    remove component_list filtering for ring forces

--- xcamshift/src/cython/Cython_shift_calculator.pyx
+++ xcamshift/src/cython/Cython_shift_calculator.pyx
@@ -2000,8 +2000,11 @@ cdef class Fast_ring_force_calculator(Base_force_calculator):
     
     
     cdef inline void _do_calc_components(self, int[:] component_to_result, float[:] force_factors, Out_array force):
-        for i in range(self._num_components):
-            self._ring_calc_single_force_set(i,force_factors[component_to_result[i]],force)
+        cdef int factor_index 
+        cdef int component_index
+        
+        for factor_index, component_index in enumerate(self._active_components):
+            self._ring_calc_single_force_set(component_index,force_factors[component_to_result[factor_index]],force)
             
     cdef inline void _ring_calc_single_force_set(self,  int index, float force_factor, Out_array forces): 
         cdef int atom_type_id  = self._compiled_components[index].atom_type_id
--- xcamshift/src/xcamshift.py
+++ xcamshift/src/xcamshift.py
@@ -841,7 +841,7 @@ class Base_potential(object):
 
                 
                 
-            if self.get_abbreviated_name() ==  DIHEDRAL:
+            if self.get_abbreviated_name() in (RING,  DIHEDRAL):
                 components = self._get_component_list().get_native_components()
                 self._force_calculator(components, self._component_to_result, force_factors, forces, active_components=self._active_components)
             else:   
