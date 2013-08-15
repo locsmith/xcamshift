40cf64525bdbd36c98439fc2d5843cb99ac41bee
varioustoxins <garyt@bmb.leeds.ac.uk>
2013-08-15 22:38:50 +0100

    add variables to define xplor installation path and xplor version
    note 1. this could be improved by auto detecting these variables by
    doing a which pyXplor
    
    Conflicts:
    	xcamshift/src/cython/setup.py

--- xcamshift/src/cython/setup.py
+++ xcamshift/src/cython/setup.py
@@ -12,7 +12,7 @@
 Created on 23 Dec 2011
 
 @author: garyt
-'''  
+''' 
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
@@ -20,48 +20,51 @@ from Cython.Distutils import build_ext
 import Cython.Compiler.Options
 Cython.Compiler.Options.annotate = True
 
+# change path and architecture to match local installation
+xplor_path ='/home/garyt/programs/xplor-nih/2.31.0'
+architecture = 'Linux_i686'
+
+
+params = {'path'         : xplor_path,
+          'architecture' : architecture}
+
+templates=[ '{path}/common',
+            '{path}/CDSlib',
+            '{path}/arch/{architecture}/include']
+include_dirs = [template.format(**params) for template in templates]
+                                       
 ext_modules = [Extension("shift_calculators",  ["Cython_shift_calculator.pyx", 'sharedCDSVectorFactory.cc'],
                          define_macros = [('CPLUSPLUS', '1') ,
                                           ('USE_CDS_NAMESPACE', '1')],
                         
                          language="c++",
                          extra_compile_args=["-O3"],
-                         
-                         include_dirs=['/home/garyt/programs/xplor-nih/2.31.0/common',
-                                       '/home/garyt/programs/xplor-nih/2.31.0/CDSlib',
-                                       '/home/garyt/programs/xplor-nih/2.31.0/arch/Linux_i686/include']),
+                         include_dirs=include_dirs),
 				       
                Extension("pyEnsemblePot",  ["PyEnsemblePotProxy.cc",'pyEnsemblePot.pyx'],   
                          define_macros = [('CPLUSPLUS', '1') ,
                                            ('USE_CDS_NAMESPACE', '1')],
                         
-                         language="c++", 
-
-                         
-                         include_dirs=['/home/garyt/programs/xplor-nih/2.31.0/common',
-                                       '/home/garyt/programs/xplor-nih/2.31.0/CDSlib',
-                                       '/home/garyt/programs/xplor-nih/2.31.0/arch/Linux_i686/include']), 
-
-
+                         language="c++",
+                         extra_compile_args=["-O3"],
+                         include_dirs=include_dirs),
+               
                Extension("fast_segment_manager",  ["Cython_segment_manager.pyx"],
                          define_macros = [('CPLUSPLUS', '1') ,
                                           ('USE_CDS_NAMESPACE', '1')],
                         
                          language="c++",
-                         
-                         include_dirs=['/home/garyt/programs/xplor-nih/2.31.0/common',
-                                       '/home/garyt/programs/xplor-nih/2.31.0/CDSlib',
-                                       '/home/garyt/programs/xplor-nih/2.31.0/arch/Linux_i686/include']),
+                         extra_compile_args=["-O3"],
+                         include_dirs=include_dirs),
+               
                
                Extension("xCamshiftEnsemble",  ["xCamshiftEnsemble.pyx"],
                          define_macros = [('CPLUSPLUS', '1') ,
                                           ('USE_CDS_NAMESPACE', '1')],
                         
                          language="c++",
-                         
-                         include_dirs=['/home/garyt/programs/xplor-nih/2.31.0/common',
-                                       '/home/garyt/programs/xplor-nih/2.31.0/CDSlib',
-                                       '/home/garyt/programs/xplor-nih/2.31.0/arch/Linux_i686/include'])
+                         extra_compile_args=["-O3"],
+                         include_dirs=include_dirs)
               ]
 
 if False:
