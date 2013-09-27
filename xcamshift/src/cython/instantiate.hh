f0f32c8306a422bbfab8298bdec8c9921bd550ac
varioustoxins <garyt@bmb.leeds.ac.uk>
2013-09-27 13:57:41 +0100

    add explicit instantiation of CDSString<char>

--- xcamshift/src/cython/instantiate.hh
+++ xcamshift/src/cython/instantiate.hh
@@ -3,10 +3,14 @@
 #include "cdsVector.cc"
 #include "vec3.hh"
 #include "ensembleSimulation.hh"
+#include "cdsString.hh"
+#include "cdsString.cc"
+
 template class CDSVector<Vec3,0,CDS::DefaultAlloc>;
 template class  CDSVectorBase<Vec3,CDS::DefaultAlloc>;
 template class CDSVector<double,0,EnsembleSimulation::SharedAlloc>;
 template class  CDSVectorBase<double,EnsembleSimulation::SharedAlloc>;
 template class CDSVector<float,0,CDS::DefaultAlloc>;
 template class  CDSVectorBase<float,CDS::DefaultAlloc>;
+//template class CDSString<char>;
 
