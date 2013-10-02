ceaeafed4735eee014cb4d13f3656124bece30b1
varioustoxins <garyt@bmb.leeds.ac.uk>
2013-10-02 22:29:39 +0100

    add in template instantiations that work for gcc x86_64
    contribution from Hoi Tik Alvin Leung this ensures compilation includes
    all the templates used in each shared object. It is most probably over
    eager in what it compiles and could cause shared library bloat. However,
    the overhead should be relatively small as shown by the shared library
    sizes:
    
    572K	_PyEnsemblePotProxy.so
    364K	pyEnsemblePot.so
    620K	_xPyPot.so
    
    this closes the ticket #43 'undefined variables in _xPyPot.so'

--- xcamshift/src/cython/instantiate.hh
+++ xcamshift/src/cython/instantiate.hh
@@ -1,16 +1,15 @@
 #include "cdsMath.hh"
 #include "cdsVector.hh"
 #include "cdsVector.cc"
-#include "vec3.hh"
-#include "ensembleSimulation.hh"
 #include "cdsString.hh"
 #include "cdsString.cc"
-
+#include "vec3.hh"
+#include "fixedVector.cc"
+#include "ensembleSimulation.hh"
 template class CDSVector<Vec3,0,CDS::DefaultAlloc>;
 template class  CDSVectorBase<Vec3,CDS::DefaultAlloc>;
 template class CDSVector<double,0,EnsembleSimulation::SharedAlloc>;
 template class  CDSVectorBase<double,EnsembleSimulation::SharedAlloc>;
 template class CDSVector<float,0,CDS::DefaultAlloc>;
 template class  CDSVectorBase<float,CDS::DefaultAlloc>;
-//template class CDSString<char>;
-
+template class CDSString<char>;
