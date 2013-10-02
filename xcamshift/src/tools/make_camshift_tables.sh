aec6bc817e65d99560bd959bbd86df8f6c5a280d
varioustoxins <garyt@bmb.leeds.ac.uk>
2013-10-02 18:37:52 +0100

    add correct output directory for building camshift data files

--- xcamshift/src/tools/make_camshift_tables.sh
+++ xcamshift/src/tools/make_camshift_tables.sh
@@ -4,5 +4,5 @@
 XCAMSHIFT_HOME=$HOME/Dropbox/git/xcamshift/xcamshift
 export PYTHONPATH=$XCAMSHIFT_HOME/src/:$XCAMSHIFT_HOME/lib/PyYAML/3.10/installation/lib/python2.7/site-packages/
 
-python2.7 $XCAMSHIFT_HOME/src/table_builders/xcamshift/camshift_table_builder.py -t $XCAMSHIFT_HOME/data/xcamshift_templates/ $XCAMSHIFT_HOME/data/camshift_1_35_0_almost_1_0_4/  
+python2.7 $XCAMSHIFT_HOME/src/table_builders/xcamshift/camshift_table_builder.py -t $XCAMSHIFT_HOME/data/xcamshift_templates/ -o $XCAMSHIFT_HOME/data $XCAMSHIFT_HOME/data/camshift_1_35_0_almost_1_0_4/  
 
