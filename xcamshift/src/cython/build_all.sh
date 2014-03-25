d43f898276c2dd3b53695bd402b4ab778c5df540
garythompson <garythompson@localhost>
2014-03-25 18:53:52 +0000

    change build files to build with osx and clang
    the last set of build files suitable for linux are in commit
    267f382901a3222d6495ab36707b75feecea5679 in the newar future I will
    amalagamate the adhoc build systems for linux and osx

--- xcamshift/src/cython/build_all.sh
+++ xcamshift/src/cython/build_all.sh
@@ -1,14 +1,15 @@
 #!/bin/sh  
+export CFLAGS=-Qunused-arguments
+export CPPFLAGS=-Qunused-arguments
 
 export xc_xplor_version='2.35.0'
-export xc_xplor_root="/home/garyt/programs/xplor-nih/${xc_xplor_version}"
-export xc_platform='Linux'
-export xc_architecture='i686'
+export xc_xplor_root="/Users/garythompson/programs/xplor-nih/${xc_xplor_version}"
+export xc_platform='Darwin_13'
+export xc_architecture='x86_64'
 export xc_python_version='2.7'
 export xc_lower_platform=$(echo "${xc_platform}" | tr '[:upper:]' '[:lower:]')
 
-export PYTHONPATH=/home/garyt/programs/cython/0.18.0
+export PYTHONPATH=/Users/garythompson/programs/cython/0.20.1
 
-./build_xpypot.sh $xc_xplor_version $xc_xplor_root $xc_platform $xc_architecture $xc_python_version
 ${xc_xplor_root}/bin/pyXplor setup.py build_ext --inplace 
-./build_extension.sh 
+#./build_extension.sh 
