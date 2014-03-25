5da153802f84f5d4aed6287cd53322c66c932d54
garythompson <garythompson@localhost>
2014-03-25 18:21:51 +0000

    add some instantiations required to build under 2.35.0 and osx
    it is not currently clear which of these changes cause this need!

--- xcamshift/src/cython/instantiate.hh
+++ xcamshift/src/cython/instantiate.hh
@@ -8,6 +8,8 @@
 #include "vec3.hh"
 #include "fixedVector.cc"
 #include "ensembleSimulation.hh"
+#include "cdsList.hh"
+#include "cdsList.cc"
 template class CDSVector<Vec3,0,CDS::DefaultAlloc>;
 template class  CDSVectorBase<Vec3,CDS::DefaultAlloc>;
 template class CDSVector<double,0,EnsembleSimulation::SharedAlloc>;
@@ -16,4 +18,4 @@ template class CDSVector<float,0,CDS::DefaultAlloc>;
 template class  CDSVectorBase<float,CDS::DefaultAlloc>;
 template class CDSString<char>;
 template class CDSStringStreamBuf<char>;
-
+template class CDSList<CDSString<char>, 10>;
