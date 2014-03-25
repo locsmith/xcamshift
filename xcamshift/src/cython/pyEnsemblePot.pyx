bf25b5e27610d42507e03a0fba669c25aa8bd65a
garythompson <garythompson@localhost>
2014-03-25 18:07:08 +0000

    remove the need to use xPyPot to access the simulation field
    this requires the following patch to python/pyPot.hh, this works with
    xplor 2.35.0
    
    41,42c41
    <
    <   Simulation * simulation() {return sim;};
    ---
    >

--- xcamshift/src/cython/pyEnsemblePot.pyx
+++ xcamshift/src/cython/pyEnsemblePot.pyx
@@ -1,6 +1,6 @@
 
 # from pyEnsemblePotProxy cimport PyEnsemblePotProxy  as  CPPPyEnsemblePotProxy
-from cython.xPyPot import PyPot
+from pyPot import PyPot
 cimport cpython.ref as cpy_ref
 from  xplor_access cimport Simulation, String, DerivList, EnsembleSimulation, EnsembleMemberSimulation, numSimulations
 from libc.stdio cimport printf
@@ -20,7 +20,7 @@ cdef class PyEnsemblePotData:
     def __init__(self, instance_name, potential_name='test'):
         self._instance_name = new String(instance_name, len(instance_name))
         self._potential_name = new String(potential_name, len(potential_name))
-        self._simulation = <Simulation*><size_t>self.simulation()
+        self._simulation = <Simulation*><size_t>int(self.simulation().this)
         self.ensemblePotProxy = new PyEnsemblePotProxy(self._instance_name[0], self._potential_name[0],self._simulation, <cpy_ref.PyObject*>self)
      
     def calcEnergyAndDerivList(self,derivList):
