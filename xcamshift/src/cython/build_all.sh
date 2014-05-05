af0da4019e44e1075d20e4d5c89e82707ca7e8db
gary thompson <garyt@bmb.leeds.ac.uk>
2014-05-05 13:28:35 +0100

    update build all to support osx as well as linux

--- xcamshift/src/cython/build_all.sh
+++ xcamshift/src/cython/build_all.sh
@@ -1,10 +1,20 @@
 #!/bin/sh  
-export CFLAGS=-Qunused-arguments
-export CPPFLAGS=-Qunused-arguments
+
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+    export CFLAGS=-Qunused-arguments
+    export CPPFLAGS=-Qunused-arguments
+elif [[ "$unamestr" == 'Darwin' ]]; then
+    export LDSHARED="/usr/local/Cellar/gcc48/4.8.2/bin/g++-4.8 -Wl,-F. -bundle -undefined dynamic_lookup" 
+    export CC=/usr/local/Cellar/gcc48/4.8.2/bin/g++-4.8 
+    export LINKCC=/usr/local/Cellar/gcc48/4.8.2/bin/g++-4.8
+    export LDCXXSHARED="/usr/local/Cellar/gcc48/4.8.2/bin/g++-4.8 -bundle -undefined dynamic_lookup"
+    export CXX=/usr/local/Cellar/gcc48/4.8.2/bin/g++-4.8 
+fi
 
 export xc_xplor_version='2.35.0'
 export xc_xplor_root="/Users/garythompson/programs/xplor-nih/${xc_xplor_version}"
-export xc_platform='Darwin_13'
+export xc_platform='Darwin_10'
 export xc_architecture='x86_64'
 export xc_python_version='2.7'
 export xc_lower_platform=$(echo "${xc_platform}" | tr '[:upper:]' '[:lower:]')
