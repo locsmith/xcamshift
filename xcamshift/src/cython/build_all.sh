d2dbc5c2c66a475021ed0ee76b89f09bbd06e5ff
gary thompson <garyt@bmb.leeds.ac.uk>
2014-09-30 14:10:17 +0200

    add comments to assist users in setting up a build of xcamshift

--- xcamshift/src/cython/build_all.sh
+++ xcamshift/src/cython/build_all.sh
@@ -12,14 +12,37 @@ elif [[ "$unamestr" == 'Darwin' ]]; then
     export CXX=/usr/local/Cellar/gcc48/4.8.2/bin/g++-4.8 
 fi
 
+# this is the version of xplor  used
 export xc_xplor_version='2.35.0'
+
+# i name my xplor directories after the version of the programs used
+# you may want a different path here
 export xc_xplor_root="/Users/garythompson/programs/xplor-nih/${xc_xplor_version}"
+
+# this is the operating system you are using:
+# for linux this should be Linux 
+# for OSX this should be Darwin_XX where XX is the version of darwin  underlying the 
+# OSX build to identify the Darwin version have a look at bin.Darwin_XX_x86_64 directory
+# in your xplor nih directory XX_Darwin be the platform and x86_64 is the architecture
 export xc_platform='Darwin_10'
-export xc_architecture='x86_64'
+
+# this is the processor  architecture you are using
+# for linux with a 64 bit processor use x86_64, with a 32 bit processor use i686 
+# for OSX with a 64 bit processor use x86_64 for a 32 bit processsor use ?not currently know?
+export xc_architecture=x86_64
+
+# this is the version of python you are compiling against
+# find it by running pyXplor
+# and type 
+# python> import sys
+# python> print sys.version 
+# version will be in the first two digits ie xx.yy in xx.yy.zz
 export xc_python_version='2.7'
+
 export xc_lower_platform=$(echo "${xc_platform}" | tr '[:upper:]' '[:lower:]')
 
+# this is where to find the cython executable
 export PYTHONPATH=/Users/garythompson/programs/cython/0.20.1
 
 ${xc_xplor_root}/bin/pyXplor setup.py build_ext --inplace 
-#./build_extension.sh 
+./build_extension.sh 
