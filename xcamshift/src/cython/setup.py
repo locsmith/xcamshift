217f381d2c97c6d40b019d58e6037a69e938ab53
gary thompson <garyt@bmb.leeds.ac.uk>
2014-11-06 07:40:17 +0000

    set a coomon variable for extra compiler arguments

--- xcamshift/src/cython/setup.py
+++ xcamshift/src/cython/setup.py
@@ -4,7 +4,7 @@
 # are made available under the terms of the GNU Lesser Public License v3.0
 # which accompanies this distribution, and is available at
 # http://www.gnu.org/licenses/lgpl-3.0.html
-# 
+#
 # Contributors:
 #     gary thompson - initial API and implementation
 #-------------------------------------------------------------------------------
@@ -12,14 +12,14 @@
 Created on 23 Dec 2011
 
 @author: garyt
-''' 
+'''
 
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 import os
- 
+
 import Cython.Compiler.Options
 Cython.Compiler.Options.annotate = True
 
@@ -29,6 +29,7 @@ architecture =    os.environ['xc_architecture']        # 'i686'
 platform =        os.environ['xc_platform']            # 'Linux'
 python_version =  os.environ['xc_python_version']      # '2.6'
 
+extra_compile_args = ["-O3"]
 
 params = {'path'           : xplor_path,
           'architecture'   : architecture,
@@ -46,29 +47,29 @@ extra_link_args =  [template.format(**params) for template in extra_link_arg_tem
 ext_modules = [Extension("shift_calculators",  ["Cython_shift_calculator.pyx",'sharedCDSVectorFactory.cc'],
                          define_macros = [('CPLUSPLUS', '1') ,
                                           ('USE_CDS_NAMESPACE', '1')],
-                        
+
                          language="c++",
-                         extra_compile_args=["-O3"],
+                         extra_compile_args=extra_compile_args,
                          extra_link_args=extra_link_args,
                          include_dirs=include_dirs),
 
-				       
-               Extension("pyEnsemblePot",  ["PyEnsemblePotProxy.cc",'pyEnsemblePot.pyx'],   
+
+               Extension("pyEnsemblePot",  ["PyEnsemblePotProxy.cc",'pyEnsemblePot.pyx'],
                          define_macros = [('CPLUSPLUS', '1') ,
                                            ('USE_CDS_NAMESPACE', '1')],
-                        
+
                          language="c++",
-                         extra_compile_args=["-O3"],
+                         extra_compile_args=extra_compile_args,
                          extra_link_args=extra_link_args,
                          include_dirs=include_dirs),
-               
+
 
                Extension("fast_segment_manager",  ["Cython_segment_manager.pyx"],
                          define_macros = [('CPLUSPLUS', '1') ,
                                           ('USE_CDS_NAMESPACE', '1')],
-                        
+
                          language="c++",
-                         extra_compile_args=["-O3"],
+                         extra_compile_args=extra_compile_args,
                          extra_link_args=extra_link_args,
                          include_dirs=include_dirs)
 
@@ -82,4 +83,4 @@ setup(
     ext_modules=ext_modules,
     cmdclass = {'build_ext': build_ext},
 )
-      
+
