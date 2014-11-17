952859a4c8df6907f2eba33cd15ad63ba96366aa
gary thompson <garyt@bmb.leeds.ac.uk>
2014-11-17 13:30:37 +0000

    xcamshift didn't build with the old xplor ensemble interace
    at some point between 2.31 and 2.35 the interface to the ensemble
    routines changed in an incompatible manner. Add some conditional
    compilation to cope with this

--- xcamshift/src/cython/PyEnsemblePotProxy.cc
+++ xcamshift/src/cython/PyEnsemblePotProxy.cc
@@ -27,18 +27,24 @@ PyEnsemblePotProxy::~PyEnsemblePotProxy()
   unRegister(esim);
 }
 
-//float_type PyEnsemblePotProxy::energyMaybeDerivs0(DerivList& derivList, bool calcDerivs){return callCyEnergyMaybeDerivs(derivList, calcDerivs, 0);}
-//float_type PyEnsemblePotProxy::energyMaybeDerivs1(DerivList &derivList, bool calcDerivs){return callCyEnergyMaybeDerivs(derivList, calcDerivs, 1);}
-//float_type PyEnsemblePotProxy::energyMaybeDerivs2(DerivList &derivList, bool calcDerivs){return callCyEnergyMaybeDerivs(derivList, calcDerivs, 2);}
-//float_type PyEnsemblePotProxy::energyMaybeDerivs3(DerivList &derivList, bool calcDerivs){return callCyEnergyMaybeDerivs(derivList, calcDerivs, 3);}
-//float_type PyEnsemblePotProxy::energyMaybeDerivs4(DerivList &derivList, bool calcDerivs){return callCyEnergyMaybeDerivs(derivList, calcDerivs, 4);}
 
+#if OLD_ENSEMBLE_INTERFACE > 0
+
+float_type PyEnsemblePotProxy::energyMaybeDerivs0(DerivList& derivList, bool calcDerivs) {return callCyEnergyMaybeDerivs(&derivList, calcDerivs, 0);}
+float_type PyEnsemblePotProxy::energyMaybeDerivs1(DerivList& derivList, bool calcDerivs) {return callCyEnergyMaybeDerivs(&derivList, calcDerivs, 1);}
+float_type PyEnsemblePotProxy::energyMaybeDerivs2(DerivList& derivList, bool calcDerivs) {return callCyEnergyMaybeDerivs(&derivList, calcDerivs, 2);}
+float_type PyEnsemblePotProxy::energyMaybeDerivs3(DerivList& derivList, bool calcDerivs) {return callCyEnergyMaybeDerivs(&derivList, calcDerivs, 3);}
+float_type PyEnsemblePotProxy::energyMaybeDerivs4(DerivList& derivList, bool calcDerivs) {return callCyEnergyMaybeDerivs(&derivList, calcDerivs, 4);}
+
+#else /* OLD_ENSEMBLE_INTERFACE */
 
 float_type PyEnsemblePotProxy::energyMaybeDerivs0(DerivList* derivList){return callCyEnergyMaybeDerivs(derivList, true, 0);}
 float_type PyEnsemblePotProxy::energyMaybeDerivs1(DerivList* derivList){return callCyEnergyMaybeDerivs(derivList, true, 1);}
 float_type PyEnsemblePotProxy::energyMaybeDerivs2(DerivList* derivList){return callCyEnergyMaybeDerivs(derivList, true, 2);}
 float_type PyEnsemblePotProxy::energyMaybeDerivs3(DerivList* derivList){return callCyEnergyMaybeDerivs(derivList, true, 3);}
 float_type PyEnsemblePotProxy::energyMaybeDerivs4(DerivList* derivList){return callCyEnergyMaybeDerivs(derivList, true, 4);}
+
+#endif /* OLD_ENSEMBLE_INTERFACE */
 
 float_type PyEnsemblePotProxy::callCyEnergyMaybeDerivs(DerivList* derivList, bool calcDerivs, int i) {
 	if (derivList == 0){
@@ -61,9 +67,21 @@ float_type PyEnsemblePotProxy::callCyEnergyMaybeDerivs(DerivList* derivList, boo
 float_type PyEnsemblePotProxy::rms(){
 	return -1.0f;
 }
+
+#if OLD_ENSEMBLE_INTERFACE > 0
+
+int PyEnsemblePotProxy::violations(){
+	return -1;
+}
+
+#else /* OLD_ENSEMBLE_INTERFACE */
+
 float_type PyEnsemblePotProxy::violations(){
 	return -1;
 }
+
+#endif /* OLD_ENSEMBLE_INTERFACE */
+
 int PyEnsemblePotProxy::numRestraints(){
 	return -1;
 }
