09b004ca8da18a6674025d1fb1a3acb9cf5a181d
gary thompson <garyt@bmb.leeds.ac.uk>
2014-11-27 09:01:21 +0000

    bug #64 if the files directory containing the force-field isn't found...
    now raises a clear exception rather than crashing if the force field
    database isn't found 
    
    close #64

--- xcamshift/src/table_manager.py
+++ xcamshift/src/table_manager.py
@@ -4,7 +4,7 @@
 # are made available under the terms of the GNU Lesser Public License v3.0
 # which accompanies this distribution, and is available at
 # http://www.gnu.org/licenses/lgpl-3.0.html
-# 
+#
 # Contributors:
 #     gary thompson - initial API and implementation
 #-------------------------------------------------------------------------------
@@ -41,6 +41,10 @@ def _build_table_manager():
 
 Cache_manager.get_cache_manager().add_cache_builder(TABLE_MANAGER,_build_table_manager)
 
+class Database_not_found_exception(Exception):
+    def __init__(self,*args,**kwargs):
+        super(Database_not_found_exception, self).__init__(*args,**kwargs)
+
 def _get_residue_type_cache():
     return  Cache_manager.get_cache(RESIDUE_TYPE)
 #TODO: cleanup internal structure, caching needs a better implementation
@@ -49,16 +53,16 @@ def _get_residue_type_cache():
 #TODO: caching should work on the wrapper object not the internal _table object
 class Table_manager(object):
 
-    
-        
+
+
     @staticmethod
     def get_default_table_manager():
         return Cache_manager.get_cache_manager().get_cache(TABLE_MANAGER)
-    
-        
+
+
     '''
-    class to load and store a set of yaml tables for the camshift forcefield, 
-    results are looked up in order favouring specific residue types 
+    class to load and store a set of yaml tables for the camshift forcefield,
+    results are looked up in order favouring specific residue types
     and then a table name
     '''
 
@@ -66,7 +70,7 @@ class Table_manager(object):
     TYPE = 'cams'
     VERSION = '1_35_0'
     DEFAULT_DIRECTORY = 'data'
-    
+
     #TODO use common constants
     BACKBONE = "bb"
     RANDOM_COIL = "rc"
@@ -79,19 +83,19 @@ class Table_manager(object):
     NON_BONDED = "nb"
     DISULPHIDE = 'disu'
     HBOND = 'hbond'
-    
+
     def __init__(self,paths=[]):
         '''
         Constructor
         '''
-        
+
         self.search_paths = paths + ['.',self.DEFAULT_DIRECTORY]
         self.tables ={}
         self._known_table_types = set()
         self._table_index = {}
-        
+
         add_access_to_yaml_list_based_keys()
-    
+
     def __str__(self):
         result = []
         result.append('table manager')
@@ -106,65 +110,65 @@ class Table_manager(object):
         key_list = ', '.join(['%s.%s' % (table,residue) for table,residue in self.tables.keys()])
         result.append('tables: %s' % key_list)
         result.append('')
-        
-        
+
+
         return '\n'.join(result)
-    
+
     def get_table_types(self):
 
         types = set([key[0]for key in self.tables.keys()])
         types = list(types)
         types.sort()
         return tuple(types)
-        
+
 
     def _make_presorted_tuple(self, result):
         result = list(result)
         result.sort()
         result = tuple(result)
-        
+
         return result
 
-    
+
     def _load_tables(self, table_type):
         for residue_type in utils.iter_residue_types():
             self._get_table(table_type, residue_type)
-            
-    
+
+
     def get_residue_types_for_table(self,table_type):
         table_type = table_type.lower()
         self._load_tables(table_type)
         residues = set([key[1] for key in self.tables.keys() if key[0] == table_type])
- 
+
         self._make_presorted_tuple(residues)
         return residues
-    
+
     #TODO: note this only works for loaded potentials
     #TODO: note the above statement is wrong and maybe should be corrected?
     #TODO: in actual fact it could just be replaced by a call to the segment manager!
     def get_all_known_residue_types(self):
-        
+
         result = set()
         for table_type in self.get_table_types():
             for residue_type in self.get_residue_types_for_table(table_type):
                 result.add(residue_type)
-        
-        
+
+
         return self._make_presorted_tuple(result)
-    
+
     def get_all_known_table_residue_types(self):
-        
+
         result  =  set(self.tables[key][RESIDUE_TYPE] for key in self.tables)
-        
+
         return self._make_presorted_tuple(result)
-    
-    
+
+
     def __get_table_name(self, table_type, residue_type):
         return self.TEMPLATE_3 % (self.TYPE,self.VERSION,table_type,residue_type)
-    
+
     def add_search_path(self,path):
         self.search_paths.insert(0, path)
-        
+
 
     def __raise_table_load_error(self, table_name, detail):
         raise IOError("ERROR: couldn't load %s because %s" % (table_name, detail))
@@ -172,21 +176,27 @@ class Table_manager(object):
 
     #TODO: this could come from the file itself...
     def _find_parent_table(self, table_type, residue_type):
-        result = None 
+        result = None
         if residue_type != BASE_TABLE:
             result = self._get_table(table_type,None)
         return result
-#    
-    
-    
+#
+
+
 
     def _register_new_table(self, new_table, key):
         table_type =  key[0]
         if new_table != None:
             self.tables[key] = new_table
         else:
-            self.tables[key]= self.tables[table_type,BASE_TABLE]
-            
+            if (table_type,BASE_TABLE) in self.tables:
+                self.tables[key]= self.tables[table_type,BASE_TABLE]
+            else:
+                table_name  = self.__get_table_name(table_type, BASE_TABLE)
+                msg =  'ERROR: table %s is missing, the path to the database is bad!\n' % table_name
+                msg += '       search path (length = %i) is: %s\n' % (len(self.search_paths),'\n'.join(self.search_paths))
+                raise Database_not_found_exception(msg)
+
         self._known_table_types.add(table_type)
 
 
@@ -202,25 +212,25 @@ class Table_manager(object):
                           }
             new_table.update(ornaments)
             self._table_index[table_type]+=1
-    
+
     #TODO: make this take a key instead
     def __load_table(self, table_type, residue_type=BASE_TABLE):
-        
+
 #        residue_types = self.__get_residue_types(residue_type)
-        
+
         new_table = None
 #        for residue_type in residue_types:
         table_search_key = table_type,residue_type
 #            print 'search for ', table_search_key
 #            print residue_type
         table_name = self.__get_table_name(table_type,residue_type)
-        
+
         for search_path in self.search_paths:
             path = os.path.join(search_path, table_name)
-            
+
             if os.path.exists(path):
                 key = (table_type,residue_type)
-                
+
                 try:
                     table_fp = open(path)
                     new_table = load(table_fp)
@@ -228,7 +238,7 @@ class Table_manager(object):
                         break
                 except Exception as detail:
                     self.__raise_table_load_error(table_name, detail)
-#                    
+#
 #        if new_table != None:
 #            break
 
@@ -237,16 +247,16 @@ class Table_manager(object):
             parent  = self._find_parent_table(table_type,residue_type)
             self._ornament_table(new_table,table_type,residue_type)
             new_table = Hierarchical_dict(new_table, parent=parent)
-                
-            
+
+
 #        print self.tables.keys()
-        return new_table       
-    
+        return new_table
+
 
     def __get_seach_keys(self,table_type, residue_type):
         result = ((table_type,residue_type),(table_type,self.BASE_TABLE))
         return result
-    
+
     #TODO: remove __'s
     def _seach_for_loaded_table(self,table_type, residue_type):
 
@@ -255,12 +265,12 @@ class Table_manager(object):
         if key in self.tables:
             result  = self.tables[key]
         return result
-    
+
     #TODO:  combine with search for loaded table and and rename
     def __search_for_table(self, table_type, residue_type):
         keys= self.__get_seach_keys(table_type, residue_type)
         tables = self.tables
-        
+
         result = None
         for key in keys:
             if key in  tables:
@@ -272,7 +282,7 @@ class Table_manager(object):
             working_directory =  os.getcwd()
             args =  search_paths, working_directory
             self.__raise_table_load_error(table_type, "the table couldn't be found in %s working directory is: %s" % args)
-            
+
         return result
 
 
@@ -285,34 +295,33 @@ class Table_manager(object):
 
     def iter_residue_types(self):
         yield(BASE_TABLE)
-        
+
         for residue_type in utils.iter_residue_types():
             yield residue_type
-            
+
     def load_tables_for_known_residues(self, table_type):
         table_type =  table_type.lower()
         # add sequence lookup delegate to allow testing (currently we need a real molecule)
         for residue_type in self.iter_residue_types():
-            
+
             residue_type = self._force_residue_type_lowercase(residue_type)
             key =  table_type,residue_type
             new_table  = self.__load_table(table_type, residue_type)
-            
+
             self._register_new_table(new_table, key)
-            
-            
+
         self._known_table_types.add(table_type)
 
 
     #TODO: this is a really inefficient method it will do lots of disk accesses, why not load all residue type tables on first call
     def _get_table(self,table_type,residue_type=None):
         residue_type = self._force_residue_type_lowercase(residue_type)
-        
+
         if table_type not in self._known_table_types:
             self.load_tables_for_known_residues(table_type)
-            
 
-        
+
+
         key= table_type,residue_type
         if key in self.tables:
             result = self.tables[key]
@@ -321,49 +330,49 @@ class Table_manager(object):
             if result == None:
                 self.__raise_table_load_error(table_type, "couldn't find table for residue type %s" % residue_type)
             self.tables[key]=result
-        
-#        self.tables.keys()
-            
 
-        
+#        self.tables.keys()
+
+
+
         return result
-            
+
     def get_BB_Distance_Table(self,residue_type):
         return Distance_table(self._get_table(self.BACKBONE, residue_type))
-    
+
     def get_random_coil_table(self, residue_type):
         return Random_coil_table(self._get_table(self.RANDOM_COIL, residue_type))
-    
+
     def get_extra_table(self,residue_type):
         return extra_table.Extra_table(self._get_table(self.EXTRA,residue_type))
-    
+
     def _get_dihedral_table(self,residue_type):
         return Dihedral_table(self._get_table(self.DIHEDRAL,residue_type))
-    
+
     def _get_dihedral_parameter_table(self,residue_type):
         return Dihedral_parameter_table(self._get_table(self.DIHEDRAL_PARS,residue_type))
-    
+
     def get_dihedral_table(self,residue_type):
         dihedral_table = self._get_dihedral_table(residue_type)
         dihedral_parameter_table =  self._get_dihedral_parameter_table(residue_type)
         return Composite_dihedral_table(dihedral_table, dihedral_parameter_table)
-    
+
     def get_sidechain_table(self,residue_type):
         return Sidechain_table(self._get_table(self.SIDECHAIN,residue_type))
-    
+
     def get_constants_table(self,residue_type):
         return Constants_table(self._get_table(self.CONSTANTS,residue_type))
-    
+
     def get_ring_table(self,residue_type):
         return Ring_table(self._get_table(self.RING,residue_type))
-    
+
     def get_non_bonded_table(self,residue_type):
         return Non_bonded_table(self._get_table(self.NON_BONDED,residue_type))
-    
+
     def get_disulphide_table(self,residue_type):
         return Disulphide_table(self._get_table(self.DISULPHIDE,residue_type))
-    
+
     def get_hydrogen_bond_table(self,residue_type):
         return Hydrogen_bond_table(self._get_table(self.HBOND,residue_type))
 
-            
+
--- xcamshift/src/test/Test_table_manager.py
+++ xcamshift/src/test/Test_table_manager.py
@@ -4,7 +4,7 @@
 # are made available under the terms of the GNU Lesser Public License v3.0
 # which accompanies this distribution, and is available at
 # http://www.gnu.org/licenses/lgpl-3.0.html
-# 
+#
 # Contributors:
 #     gary thompson - initial API and implementation
 #-------------------------------------------------------------------------------
@@ -15,7 +15,7 @@ Created on 30 Dec 2011
 @author: garyt
 '''
 import unittest2
-from table_manager import Table_manager, TABLE_MANAGER
+from table_manager import Table_manager, TABLE_MANAGER, Database_not_found_exception
 from keys import Atom_key, Dihedral_key
 from protocol import initStruct
 from pdbTool import PDBTool
@@ -25,8 +25,8 @@ from cython.fast_segment_manager import Segment_Manager
 
 
 class Test_table_manager(unittest2.TestCase):
-    raw_dihderal_keys = ((("C", -1), ("N", 0), ("CA", 0), ("C",  0)), 
-                         (("N",  0), ("CA", 0), ("C", 0), ("N",  1)), 
+    raw_dihderal_keys = ((("C", -1), ("N", 0), ("CA", 0), ("C",  0)),
+                         (("N",  0), ("CA", 0), ("C", 0), ("N",  1)),
                          (("N", 0), ("CA", 0), ("CB", 0), ("CG", 0)))
     TARGET_ATOMS = ["HA", "CA", "HN", "N", "C", "CB"]
 
@@ -35,7 +35,7 @@ class Test_table_manager(unittest2.TestCase):
             sequence_strings = [`elem` for elem in sequence]
             sequence_string = '\n'.join(sequence_strings)
             raise AssertionError("element %s not found in the sequence:\n%s" % sequence_string,`expected`)
-        
+
     def setUp(self):
         # TODO add a delegate to get a sequence to the table manager and remove dependance on a psf file
         initStruct("test_data/gb3/gb3.psf")
@@ -50,142 +50,142 @@ class Test_table_manager(unittest2.TestCase):
         self.assertTrue(table != None)
 
     def testLoadTableValues(self):
-        table = self.table_manager.get_BB_Distance_Table('glu') 
-        
+        table = self.table_manager.get_BB_Distance_Table('glu')
+
         exponent = table.get_exponent()
         self.assertAlmostEqual(exponent,1.0)
-        
+
         coefficient = table.get_distance_coeeficent('HA', -1, 'N')
         self.assertAlmostEqual(coefficient,0.10495380090732243)
-        
+
         coefficient = table.get_distance_coeeficent('HA', -1, 'CA')
         self.assertAlmostEqual(coefficient,None)
-        
-    
+
+
     def testOffsets(self):
-        table = self.table_manager.get_BB_Distance_Table('glu') 
+        table = self.table_manager.get_BB_Distance_Table('glu')
         offsets = table.get_offsets()
-        
+
         expected = set((-1,0,1))
         self.assertItemsEqual(expected,offsets)
-        
+
     def testFromAtomList(self):
-        table = self.table_manager.get_BB_Distance_Table('glu') 
+        table = self.table_manager.get_BB_Distance_Table('glu')
         from_atom_list = table.get_target_atoms()
         expected = set(('N','HN','CA','HA','C','CB'))
         self.assertItemsEqual(expected, from_atom_list)
 
     def testToAtomList(self):
-        table = self.table_manager.get_BB_Distance_Table('glu') 
+        table = self.table_manager.get_BB_Distance_Table('glu')
         to_atom_list = table.get_to_atoms()
         expected = set(('N','HN','CA','HA','C','O'))
         self.assertItemsEqual(expected, to_atom_list)
-    
+
     def testLoadRandomCoil(self):
         table = self.table_manager.get_random_coil_table('ALA')
-        
+
         expected = 8.24
         shift = table.get_random_coil_shift( 0, 'ALA','HN')
         self.assertAlmostEqual(expected, shift)
-        
+
     def testGetDefaultTableManager(self):
         table_manager = Table_manager.get_default_table_manager()
-        
+
         self.assertIsInstance(table_manager, Table_manager)
-        
+
     def testLoadExtra(self):
 
         table = self.table_manager.get_extra_table('ALA')
-        
-        
+
+
 #        key_1 = xcamshift.Extra_potential.Atom_key(0,"H")
 #        table.get_extra_shift(0,"HN",0,"HA","HN")
-        
+
         target_atoms = "HA","CA", "HN", "N", "C", "CB"
-        
+
         atoms_1 =    "HN", "HN", "HN", "C", "C", "C", "O", "O", "O", "N", "N", "N", "O", "O", "O", "N", "N", "N", "CG", "CG", "CG", "CG", "CG", "CG", "CG", "CA"
         offsets_1 =  0, 0, 0, -1, -1, -1, 0, 0, 0, 1, 1, 1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1, -1
         atoms_2 =    "HA", "C", "CB", "HA", "C", "CB", "HA", "N", "CB", "HA", "N", "CB", "HA", "N", "CB", "HA", "N", "CB", "HA", "N", "C", "C", "N", "CA", "CA", "CA"
         offets_2 =   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, 0, 0, 0, -1, 1, 0, 0, 1
-            
-            
-            
+
+
+
         for atom_1,offset_1,atom_2,offset_2 in zip(atoms_1,offsets_1,atoms_2,offets_2):
-            
+
             key_1 =  Atom_key(offset_1,atom_1)
             key_2 = Atom_key(offset_2,atom_2)
             for target_atom in target_atoms:
                 extra  = table.get_extra_shift(target_atom,key_1,key_2)
-                
-                self.assertIsNotNone(extra)
-                
 
-        
+                self.assertIsNotNone(extra)
+
+
+
     def testLoadDihedral(self):
 
         table = self.table_manager.get_dihedral_table('ALA')
-        
+
         for raw_key in self.raw_dihderal_keys:
             for target_atom in ["HA",  "CA", "HN", "N", "C", "CB"]:
                 keys = [Atom_key(offset,atom) for atom,offset in raw_key]
-                 
+
                 dihedral_key = Dihedral_key(*keys)
-                table.get_dihedral_shift(target_atom, dihedral_key) 
+                table.get_dihedral_shift(target_atom, dihedral_key)
 
     def raw_key_to_dihedral_key(self, raw_key):
         keys = [Atom_key(offset, atom) for atom, offset in raw_key]
         dihedral_key = Dihedral_key(*keys)
         return dihedral_key
-                
+
     def testGetDihdedralKeys(self):
         table = self.table_manager.get_dihedral_table('ALA')
-        
+
         expected = set (self.raw_dihderal_keys)
-        
+
         for atom_key in table.get_dihedral_keys():
             raw_key = tuple([(key.atom,key.offset) for key in atom_key])
             self.assertIn(raw_key, expected)
         self.assertEqual(len(expected),len(table.get_dihedral_keys()) )
-    
-    def testGetDihedral(self):        
+
+    def testGetDihedral(self):
         table = self.table_manager._get_dihedral_table('ALA')
-         
+
         dihedral_key = self.raw_key_to_dihedral_key(self.raw_dihderal_keys[0])
-            
+
         result = table.get_dihedral_shift("HA",dihedral_key)
         self.assertAlmostEqual(result, 0.39606482662562)
-        
+
 
     def testLoadDihedralParameters(self):
 
         table = self.table_manager.get_dihedral_table('ALA')
-        
-        
-        
+
+
+
         for raw_key in self.raw_dihderal_keys:
             for target_atom in self.TARGET_ATOMS:
                 dihedral_key = self.raw_key_to_dihedral_key(raw_key)
                 for parameter in range(5):
-                 
+
                     table.get_parameter(target_atom, dihedral_key, parameter)
-            
-    def testGetDihedralParameter(self):        
+
+    def testGetDihedralParameter(self):
         table = self.table_manager.get_dihedral_table('ALA')
-         
+
         dihedral_key = self.raw_key_to_dihedral_key(self.raw_dihderal_keys[0])
-            
+
         result = table.get_parameter("HA",dihedral_key,0)
         self.assertAlmostEqual(result, 0.3)
-    
+
     def testGetSidechainTable(self):
-        table = self.table_manager.get_sidechain_table('ALA')   
-        
-        self.assertAlmostEqual(table.get_exponent(), 1.0) 
+        table = self.table_manager.get_sidechain_table('ALA')
+
+        self.assertAlmostEqual(table.get_exponent(), 1.0)
         self.assertIn("ALA", table.get_residue_types())
         self.assertIn("CB", table.get_sidechain_atoms("ALA"))
         self.assertAlmostEqual(table.get_sidechain_coefficient("ALA", "CA", "CB"),-3.4713212)
         self.assertSequenceEqual(self.TARGET_ATOMS, table.get_target_atoms())
-    
+
     def testGetConstantTable(self):
         table  =  self.table_manager.get_constants_table('ALA')
         self.assertAlmostEqual(0.4, table.get_flat_bottom_constant())
@@ -193,54 +193,54 @@ class Test_table_manager(unittest2.TestCase):
         self.assertAlmostEqual(20.000000,table.get_end_harmonic("CA"))
         self.assertAlmostEqual(2.360000,table.get_scale_harmonic("CA"))
         self.assertAlmostEqual(1.0, table.get_weight("CA"))
-        
+
     def testGetRingTable(self):
         table = self.table_manager.get_ring_table('ALA')
-        
+
         result = table.get_ring_coefficient("CA", "PHE", "6")
         self.assertAlmostEqual(0.01072141*1000,result)
-        
+
         EXPECTED_RING_RESIDUES = ("HIS","PHE","TRP","TYR")
         result = table.get_residue_types()
         result.sort()
         self.assertSequenceEqual(result,EXPECTED_RING_RESIDUES)
-        
+
         EXPECTED_RING_TYPES = (("HIS",("5",)),("PHE",("6",)),("TRP",("5","6")),("TYR",("6",)))
         for residue_type,expected_ring_types in EXPECTED_RING_TYPES:
             ring_types = table.get_ring_types(residue_type)
             ring_types.sort()
-            
+
             self.assertSequenceEqual(ring_types, expected_ring_types)
-    
+
     def testGetNonBondedTable(self):
         table = self.table_manager.get_non_bonded_table('ALA')
-        
+
         result = table.get_non_bonded_coefficient("CA", "sphere_1", "S", "SP3")
-        
+
         self.assertAlmostEqual(result, -101.81223165473585)
-        
+
         EXPECTED_SPHERE_TYPES =  ('sphere_1','sphere_2')
         spheres =   table.get_spheres()
         self.assertSequenceEqual(EXPECTED_SPHERE_TYPES, spheres)
-        
-        EXPECTED_COEFFICIENT_KEYS = [('C', 'SP3'),('H', 'None'), ('N', 'SP3'), 
+
+        EXPECTED_COEFFICIENT_KEYS = [('C', 'SP3'),('H', 'None'), ('N', 'SP3'),
                                      ('O', 'SP3'), ('S', 'SP3'),('C', 'SP2'),
                                      ('N', 'SP2'),('O', 'SP2')]
         EXPECTED_COEFFICIENT_KEYS.sort()
         EXPECTED_COEFFICIENT_KEYS =  tuple(EXPECTED_COEFFICIENT_KEYS)
-        
+
         coefficient_keys = table.get_remote_atom_types('sphere_1')
         self.assertSequenceEqual(EXPECTED_COEFFICIENT_KEYS,coefficient_keys)
-        
+
         coefficient_keys = table.get_remote_atom_types('sphere_2')
         self.assertSequenceEqual(EXPECTED_COEFFICIENT_KEYS,coefficient_keys)
-        
+
         exponent = table.get_exponent('sphere_1')
         self.assertAlmostEqual(exponent, -3.0)
-        
+
         exponent = table.get_exponent('sphere_2')
         self.assertAlmostEqual(exponent, 1.0)
-        
+
         chem_types = table.get_chem_types()
         chem_types.sort()
         expected_chem_types =  ['C',  'CA', 'CB', 'CC', 'CN', 'CP', 'CR', 'CT',
@@ -248,88 +248,94 @@ class Test_table_manager(unittest2.TestCase):
                                 'NH2','NH3','O',  'OC', 'OH', 'S','HA','HC','H']
         expected_chem_types.sort()
         self.assertSequenceEqual(chem_types, expected_chem_types)
-        
+
         translation = table.get_chem_type_translation("CA")
         self.assertEqual(translation, ('C','SP2'))
-        
+
         translation = table.get_chem_type_translation("NH1")
         self.assertEqual(translation, ('N','SP3'))
-        
+
     def testGetDisulphideTable(self):
         initStruct("test_data/acaggaca/acaggaca.psf")
         table = self.table_manager.get_disulphide_table('CYS')
-        
+
         self.assertAlmostEqual(table.get_disulphide_shift('HA'),0.01155515)
-        
+
         self.assertSequenceEqual(table.get_residue_types(), ['CYS'])
 
-        
+
     def test_table_hierachy(self):
         table = self.table_manager.get_random_coil_table('base')
         self.assertEqual(table._table['residue_type'], 'base',)
-        
+
         table = self.table_manager.get_random_coil_table('GLY')
         self.assertEqual(table._table['residue_type'], 'base',)
-        
+
         table = self.table_manager.get_non_bonded_table('base')
         self.assertEqual(table._table['residue_type'], 'base',)
-        
+
         table = self.table_manager.get_non_bonded_table('GLY')
         self.assertEqual(table._table['residue_type'], 'gly',)
-        
+
         expected_exponent =  -3.0
         exponent = table.get_exponent('sphere_1')
         self.assertAlmostEqual(expected_exponent, exponent)
-        
+
         expected_coefficent = -90.04553541
         coefficient = table.get_non_bonded_coefficient("CA", "sphere_1", "S", "SP3")
         self.assertAlmostEqual(expected_coefficent, coefficient)
-    
+
     #TODO caching shouldn't be on the _table value but on the wrapper class
     def test_caching(self):
         table_1 = self.table_manager.get_non_bonded_table('GLY')
         table_2 = self.table_manager.get_non_bonded_table('GLY')
         self.assertEqual(id(table_1._table), id(table_2._table))
-        
-    
+
+
     def test_hydrogen_bond_table(self):
-        
+
         table = self.table_manager.get_hydrogen_bond_table('ALA')
-#         
+#
         self.assertSequenceEqual(('HA', 'CA', 'HN', 'N', 'C', 'CB'), table.get_target_atoms())
-        
+
         self.assertSequenceEqual(table.get_donor_types(),['HON'])
         self.assertSequenceEqual(table.get_acceptor_types(),['ON'])
-        
+
         self.assertSequenceContains(['.','C', 'O'], table.get_atom_selector('ON'))
         self.assertSequenceContains(['.','N', 'HN'], table.get_atom_selector('HON'))
-        
+
         with self.assertRaises(KeyError):
             table.get_atom_selector('J')
-        
+
         expected_energy_terms = {
-                    'p1': -0.40353099999999997, 
-                    'p2': 13.9148, 
-                    'p3': -0.42172500000000002, 
-                    'p4': -3.0801599999999998, 
-                    'p5':  0.98725099999999999, 
-                    'p6':  0.0, 
-                    's': -16.5, 
+                    'p1': -0.40353099999999997,
+                    'p2': 13.9148,
+                    'p3': -0.42172500000000002,
+                    'p4': -3.0801599999999998,
+                    'p5':  0.98725099999999999,
+                    'p6':  0.0,
+                    's': -16.5,
                     'r': 3.0}
-        
+
         self.assertDictEqual(table.get_energy_terms('HON','ON','DIST'),expected_energy_terms)
 
         expected_energy_term_offsets = [(-1, 'O'), (0, 'HN'), (0, 'O'), (1, 'HN')]
         self.assertSequenceEqual(table.get_energy_term_offsets(),expected_energy_term_offsets)
-        
+
         self.assertAlmostEqual(-0.00000002820/table.get_energy_offset_correction((-1, 'O'),'DIST','HA'),1.0)
-        
+
         table = self.table_manager.get_hydrogen_bond_table('GLY')
-        
+
         self.assertDictEqual(table.get_energy_terms('HON','ON','DIST'),expected_energy_terms)
-        
+
         self.assertAlmostEqual(-0.00000029080/table.get_energy_offset_correction((-1, 'O'),'DIST','HA'), 1.0)
-        
+
+    def test_empty_search_path(self):
+        self.table_manager.search_paths = []
+        with self.assertRaises(Database_not_found_exception):
+            print self.table_manager.get_hydrogen_bond_table('ALA')
+
+
 if __name__ == "__main__":
 #     unittest2.main()
     unittest2.main(module='test.Test_table_manager')
