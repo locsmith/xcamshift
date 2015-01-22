039e74cb579e774fbaf7f91c80f487cdb55765ed
gary thompson <garyt@bmb.leeds.ac.uk>
2015-01-22 20:46:05 +0000

    observed shifts now has a __len__ to give the number of shift restraints

--- xcamshift/src/observed_chemical_shifts.py
+++ xcamshift/src/observed_chemical_shifts.py
@@ -4,7 +4,7 @@
 # are made available under the terms of the GNU Lesser Public License v3.0
 # which accompanies this distribution, and is available at
 # http://www.gnu.org/licenses/lgpl-3.0.html
-# 
+#
 # Contributors:
 #     gary thompson - initial API and implementation
 #-------------------------------------------------------------------------------
@@ -29,15 +29,15 @@ class Observed_shift_table(object):
             self._chemical_shifts =  self._process_xplor_shifts(shift_data)
         else:
             raise Exception("unexpected shift format %s" % format)
-            
+
         self._native_shifts = None
-    
+
     def _process_xplor_shifts(self,shift_data):
         result = {}
         for datum in shift_data:
             result[datum.atom_id] = datum.shift
         return result
-             
+
     def _process_observed_shift_map(self,shift_data):
         result  = {}
         for key in shift_data:
@@ -51,25 +51,25 @@ class Observed_shift_table(object):
                 for atom in atoms:
                     result[atom.index()] = shift_data[key]
             else:
-                template = """key with unexpected length should be either 'segid,residue_no,atom_name' 
+                template = """key with unexpected length should be either 'segid,residue_no,atom_name'
                               or 'residue_no,atom_name' but i got '%s'"""
                 msg = template % `key`
                 raise Exception(msg)
-                    
+
         return result
-    
+
     def get_chemical_shift(self,atom_index):
         return self._chemical_shifts[atom_index]
-    
+
     def get_atom_indices(self):
         return self._chemical_shifts.keys()
-    
+
     def get_indices_for_atom_id(self):
         result ={}
         for i, atom_id in enumerate(self._chemical_shifts.keys()):
             result[atom_id] = i
         return result
-        
+
     #TODO add more generic dump capabilities
     def dump_observed_shifts(self):
         results = []
@@ -79,20 +79,22 @@ class Observed_shift_table(object):
             sub_result.append(Atom_utils._get_atom_info_from_index(atom_index))
             sub_result.append(self._chemical_shifts[atom_index])
         return tupleit(results)
-    
+
     def get_native_shifts(self, target_atom_ids):
         if self._native_shifts == None:
             self._native_shifts = allocate_array(len(target_atom_ids),'f')
             for i,target_atom_id in enumerate(target_atom_ids):
                 self._native_shifts[i] = self.get_chemical_shift(target_atom_id)
-                
+
         return self._native_shifts
-            
-    
-    def __str__(self): 
-        
+
+    def __len__(self):
+        return len(self._chemical_shifts)
+
+    def __str__(self):
+
         result  = ["shift table", "-----------",""]
         for (segid,resid,atom_name),shift in self.dump_observed_shifts():
             result.append("[%4s]:%i@%s  %7.4f" % (segid,resid,atom_name,shift))
         return "\n".join(result)
-            
+
